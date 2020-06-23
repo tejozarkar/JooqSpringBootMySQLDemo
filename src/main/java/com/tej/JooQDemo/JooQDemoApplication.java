17817d53bd4bd470d31cf895d4cd9de0468ef495
Tejas Ozarkar <tejozarkar@gmail.com>
2020-06-23 19:08:19 +0530

    Initial Spring boot project

---
+++ src/main/java/com/tej/JooQDemo/JooQDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tej.JooQDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooQDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooQDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tej/JooQDemo/JooQDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tej.JooQDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooQDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
